0cc9bca735f704c3dfd73e600ef64b3e7bb3646a
Luis Gustavo Santarosa Pinto <gustavo-computacao@hotmail.com>
2020-03-04 23:57:56 -0300

    teste
    teste

--- source/Config.php
+++ source/Config.php
@@ -4,12 +4,3 @@ define("ROOT", "http://diario.snapsolutions.com.br");
 
 define("SITE", "#diario");
 
-
-
-function url(string $uri = null): string{
-    if($uri){
-        return ROOT."/{$uri}";
-    }
-
-    return ROOT;
-}
